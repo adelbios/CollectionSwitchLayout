0129a9fe1c987f62bb7bddc4f42e11c714a13957
adelbios <adelbios@outlook.com>
2017-07-05 21:19:47 +0300

    Initial Commit

---
+++ CollectionSwitchLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionSwitchLayout
+//
+//  Created by adelRadwan on 7/5/17.
+//  Copyright © 2017 adelRadwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
